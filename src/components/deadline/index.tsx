bc9f0c3505e4f2593262570e144827c4a8d943e3
gineff <canone@inbox.ru>
2025-02-11 18:55:34 +0300

    ft: add deadline

---
+++ src/components/deadline/index.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import { TaskRequestParams } from '@/app/react-hook-form/page';
+import { Controller, useFormContext } from 'react-hook-form';
+import TextField from '../text-field';
+
+interface DeadlineProps {
+  parentTheme?: 'light' | 'dark';
+  deadline_key: 'deadline_days' | 'rules.deadline_days'
+}
+
+export const Deadline: FC<DeadlineProps> = ({ parentTheme = 'light', deadline_key }) => {
+  const { control, trigger } = useFormContext<TaskRequestParams>();
+
+  return (
+    <>
+      <Controller
+        name={deadline_key}
+        control={control}
+        rules={{
+          required: '1 и более дней',
+        }}
+        render={({ field, fieldState }) => (
+          <TextField
+            type="number"
+            label="Дедлайн (дней)"
+            min={1}
+            errorMessage={fieldState.error?.message}
+            {...field}
+            parentTheme={parentTheme}
+            onChange={(e) => {
+              field.onChange(e);
+              trigger(deadline_key);
+            }}
+          />
+        )}
+      />
+    </>
+  );
+};
